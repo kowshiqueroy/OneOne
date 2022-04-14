0a20101c58fb755dac84dcf4b0545e7460622fb2
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2022-04-14 23:46:20 +0600

    test

--- app/src/main/java/com/example/oneone/MainActivity.java
+++ app/src/main/java/com/example/oneone/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.oneone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1;
+        TextView t1;
+
+        t1=findViewById(R.id.t1);
+
+        b1= findViewById(R.id.b1);
+
+
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                t1.setText(String.format("%d", counter));
+                if (counter%2==0) {
+
+                    b1.setText("Sumiya");
+                } else {
+
+                    b1.setText("Jahid");
+                }
+
+
+            }
+        });
+
+
+
     }
 }
